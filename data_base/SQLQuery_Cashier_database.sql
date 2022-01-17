e9011bd6dc4c67b75ac18749f27a6cf29acc577a
luissantananet <luissantananet@gmail.com>
2022-01-17 16:38:44 -0300

    update 2

--- data_base/SQLQuery_Cashier_database.sql
+++ data_base/SQLQuery_Cashier_database.sql
@@ -10,16 +10,21 @@ create table tbllogin(
     senha VARCHAR(50) not null,
     primary key(idlogin)
 );
-create table tbllancamento(
-	idlanc int not null auto_increment,
+create table tbllancitem(
+	iditem int not null auto_increment,
     doc varchar(20),
     datah date,
     descricao varchar(150),
     tipo varchar(10),
     valor decimal(8,2),
+    primary key(iditem)
+);
+create table tbllacamento(
+	idlanc int not null auto_increment,
+    ddata date not null,
+    descricao varchar(100),
     primary key(idlanc)
 );
-
 -- --------------------------------------------------------
 --  Inserido dados na tabelas
 -- --------------------------------------------------------
